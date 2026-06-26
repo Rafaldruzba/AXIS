2dc1b161546530ce4d1da6c49edd46491c3d0ffa
rafaldruzba1 <rafaldruzba@op.pl>
2026-06-26 21:26:43 +0200

    changing n8n url

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.post('/api/send', async (req, res) => {
 	try {
 		const response = await fetch(
 			// testURL https://n8n-production-8616.up.railway.app/webhook-test/129f8d6c-4d99-49f0-8d7c-1d6ddccfe58b
-			'https://n8n-production-8616.up.railway.app/webhook-test/129f8d6c-4d99-49f0-8d7c-1d6ddccfe58b',
+			'https://n8n-production-8616.up.railway.app/webhook/129f8d6c-4d99-49f0-8d7c-1d6ddccfe58b',
 			{
 				method: 'POST',
 				headers: { 'Content-Type': 'application/json' },
